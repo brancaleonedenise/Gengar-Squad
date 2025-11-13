63745672055bc98529a584e3e06e3b8b6f65c02c
Denise Brancaleone <brancaleonedenise>
2025-11-13 17:18:14 +0100

    import

---
+++ pre-processing/import.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import json
+import warnings
+from tqdm.auto import tqdm
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression   # <-- Il tuo modello
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, RobustScaler # <-- StandardScaler è incluso
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import accuracy_score
+from sklearn.exceptions import ConvergenceWarning
+from xgboost import XGBClassifier
+from sklearn.ensemble import StackingClassifier
+from sklearn.model_selection import cross_val_score
