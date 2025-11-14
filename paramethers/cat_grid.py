ea70b11220ae22a71a5317b14178af1878776860
kayo-ko <kayo_yoko1@hotmail.com>
2025-11-15 00:39:41 +0100

    deleting unused files and updating parameters

--- paramethers/cat_grid.py
+++ paramethers/cat_grid.py
@@ -1,10 +1,11 @@
-# params/catboost_params.py
+# params/catboost_params_pipeline.py
 
 param_grid = {
-    'depth': [4, 5, 6, 7, 8, 9, 10],
-    'learning_rate': [0.01, 0.03, 0.05, 0.08, 0.1, 0.15, 0.2],
-    'iterations': [200, 400, 600, 800, 1000, 1200],
-    'l2_leaf_reg': [1, 3, 5, 7, 10, 12],
-    'random_seed': [42],  # fixed
-    'task_type': ['CPU'],  # fixed
+    'classifier__depth': [4, 5, 6, 7, 8, 9, 10],
+    'classifier__learning_rate': [0.01, 0.03, 0.05, 0.08, 0.1, 0.15, 0.2],
+    'classifier__iterations': [200, 400, 600, 800, 1000, 1200],
+    'classifier__l2_leaf_reg': [1, 3, 5, 7, 10, 12],
+    'classifier__random_seed': [42],  # fixed
+    'classifier__task_type': ['CPU'],  # fixed
+    'classifier__verbose': [0]  # optional: silence output during training
 }
--- paramethers/gb_grid.py
+++ paramethers/gb_grid.py
@@ -1,9 +1,10 @@
+# params/gradientboost_params.py
 param_grid = {
     'n_estimators': [200, 300, 400, 500],
     'learning_rate': [0.01, 0.05, 0.1, 0.2],
     'max_depth': [3, 4, 5, 6],
     'min_samples_split': [2, 4, 6, 10],
     'min_samples_leaf': [1, 2, 4],
-    'subsample': [0.6, 0.8, 1.0],
-    'max_features': ['sqrt', 0.5, 0.7, None]
+    'max_features': ['sqrt', 0.5, 0.7, None],
+    'subsample': [0.6, 0.8, 1.0]  # optional, for stochastic gradient boosting
 }
